6b7f6c75e9aeb2e4b436490271691375f76f0dca
gortanian <gortanian@gmail.com>
2022-10-17 21:59:42 -0700

    Make displaay capable of multi-digit numbers

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-let displayValue = "hello"; 
+let displayValue = "hello";
 
 updateDisplay(displayValue);
 
-// update the display when a button is pressed
-let buttons = document.querySelectorAll("button"); 
-for (button of buttons) {
-    button.addEventListener("click", function (e) {
-        displayValue = e.target.textContent;
-        updateDisplay(e.target.textContent);
-    })
-}
+buttonPressHandler();
+
+// TODO
+// make the display capable of multi-digit numbers
+// store the calculator items in an array 
+// when = is clicked, calculate the things and stuff
+// when C is clicked, clear out the array and display
 
 
 function add(num1, num2) {
@@ -23,7 +22,7 @@ function subtract(num1, num2) {
 function multiply(num1, num2) {
     return num1 * num2;
 }
- 
+
 function divide(num1, num2) {
     return num1 / num2;
 }
@@ -47,6 +46,28 @@ function operate(num1, operator, num2) {
 }
 
 function updateDisplay(displayValue) {
-    const display = document.getElementById("display"); 
-    display.textContent = displayValue; 
+    const display = document.getElementById("display");
+    display.textContent = displayValue;
+}
+
+function buttonPressHandler() {
+    // update the display when a button is pressed
+    let buttons = document.querySelectorAll("button");
+    for (button of buttons) {
+        button.addEventListener("click", function (e) {
+            let userItem = e.target.textContent;
+            if (isNaN(userItem)) {
+                displayValue = userItem;
+                updateDisplay(e.target.textContent);
+            }
+            else {
+                if (isNaN(displayValue)) {
+                    displayValue = '';
+                }
+                displayValue += userItem;
+                updateDisplay(displayValue);
+            }
+            
+        })
+    }
 }
